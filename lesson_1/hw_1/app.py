00152e9d6c5449f053cb00d120d64108481bd143
Rail <railbolo@gmail.com>
2024-01-15 21:56:38 +0400

    add catalog and pig 2

--- lesson_1/hw_1/app.py
+++ lesson_1/hw_1/app.py
@@ -3,13 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index/')
+@app.route('/')
 def index():
-    context = {
-        'title': 'Эластико',
-    }
-    return render_template('base.html', **context)
+    context = {'title': 'Эластико'}
+    return render_template('main.html', **context)
 
+
+@app.route('/catalog/')
+def catalog():
+    context = {'title': 'Каталог'}
+    return render_template('catalog.html', **context)
+
+@app.route('/products/')
+def product():
+    context = {'title': 'Порося'}
+    return render_template('products.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
