55502a639a3587b3d987042c25772fad2a434620
Rail <railbolo@gmail.com>
2024-01-16 22:44:21 +0400

    end hw 1

--- lesson_1/hw_1/app.py
+++ lesson_1/hw_1/app.py
@@ -14,10 +14,24 @@ def catalog():
     context = {'title': 'Каталог'}
     return render_template('catalog.html', **context)
 
+
 @app.route('/products/')
 def product():
     context = {'title': 'Порося'}
     return render_template('products.html', **context)
+
+
+@app.route('/contacts/')
+def contacts():
+    context = {'title': 'Контакты'}
+    return render_template('contacts.html', **context)
+
+
+@app.route('/feedback/')
+def feedback():
+    context = {'title': 'Обратная связь'}
+    return render_template('feedback.html', **context)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
